27de8736fbd88bfcd2f7fda70e6811f83f8b4822
HLong <hailongkc1203@gmail.com>
2024-05-31 06:41:14 +0700

    loading books from the store

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 import { AddBook, RemoveBook } from '../books/book.actions';
 
@@ -12,8 +13,13 @@ import { AddBook, RemoveBook } from '../books/book.actions';
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
+  books$: Observable<Book[]>;
 
-  constructor(private store: Store<{ books: Book[] }>) { }
+
+
+  constructor(private store: Store<{ books: Book[] }>) {
+    this.books$ = store.pipe(select('books'))
+  }
 
   addBook(id: string, title: string, author: string) {
     this.store.dispatch(AddBook({ id, title, author }))
