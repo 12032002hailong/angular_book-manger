5e1c29118e443e330b3655a8b44ae9f54de5dbcd
HLong <hailongkc1203@gmail.com>
2024-05-31 06:38:24 +0700

    creating component book-list and dispatch actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './books/book.reducer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StoreModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
